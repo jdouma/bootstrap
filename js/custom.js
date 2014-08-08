bf66b5e0123c588e2a493fa718b140f37022b227
Justin Douma <jdouma@catalystitservices.com>
2014-08-08 16:36:40 -0700

    Committing all changes indescriminately
    - didn't add new node js files

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,14 @@
 	(function () {
 		"use strict";
 
+		// turn on tooltips and popovers
+		$(function () { $("[data-toggle='tooltip']").tooltip(); });
+		$(function () { $("[data-toggle='popover']").popover(); });
+
+
+		
 		var $pickButton = $('#pickButton');
+		var $activityTableBody = $('tbody');
 
 		$("#reasonDropdown li a").on("click", function() {
 			var reason = $(this).text();
@@ -28,28 +35,67 @@
 
 		$('#theCarousel').carousel();
 
-		$('#contactForm input[type=submit]').tooltip();
+		$(document).ready(function() {
+			$('#headerContainer').load('html/header.html');
+			$('#footerContainer').load('html/footer.html');
+			$('#thingsDropdown li').on('click', function() {
+				if (! ( $(this).hasClass('disabled') || 
+					$(this).hasClass('divider'))) {
+					updateThings($(this).text());
+			}
+		});
 
 
+//
+$('.popover-markup > .trigger').popover({
+	html : true,
+	title: function() {
+		return $(this).parent().find('.head').html();
+	},
+	content: function() {
+				//return $(this).parent().find('.head').html();
+				return $(this).parent().find('#popoverContent').html();
+			},
+			container: 'body',
+			placement: 'right'
+		});
 
-	})();
 
-	// window.onload = function() {
-	// 		alert('Window loaded');
-	// 	};
+});
 
-	$(document).ready(function() {
-		$('#headerContainer').load('html/header.html');
-		$('#footerContainer').load('html/footer.html');
-		$('#thingsDropdown li').on('click', function() {
-			if (! ( $(this).hasClass('disabled') || 
-				$(this).hasClass('divider'))) {
-				updateThings($(this).text());
+		// var myJson = {};
+		// $.getJSON('http://localhost:8080/webapi/activities/1235', function(json) {
+		// 	alert("Test: " + json.duration);
+		// });	
+
+	var callback = new Function();
+	var url = "http://localhost:8080/webapi/activities/1235";
+	$.ajax( 'http://localhost:8080/webapi/activities/1235',
+	{ 	
+		 dataType:'jsonp json',
+		 jsonpCallback: 'callback',
+		//complete: function(data) { alert(JSON.stringify(data)); },
+		error: function(xhr, item2, err) {
+			alert("Error: " + JSON.stringify(xhr));
+		},
+		success: function(data) {
+			alert(data.duration);
 		}
-	})
 	});
+
+	createActivityRow("justin", "Fishing", 21);
+
+	function callback(object) {
+		alert(object);
+	}
+
+	function createActivityRow(user, activity, minutes) {
+		$activityTableBody.append('<tr class="danger"><td>' + user +  '</td><td>' + activity + 
+			'</td><td>' + minutes + '</td></tr>');
+	}	
 
 	function updateThings(label) {
 		$('#thingsButton').text(label);
 	}
 
+})();
